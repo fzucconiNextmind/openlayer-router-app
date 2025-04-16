315de39e9390c8d9b3630e448308a5bf9a8c13d9
FrancescoZucconi <f.zucconi@nextmind.it>
2025-04-16 11:29:13 +0200

    Refactor OpenLayerMap to use waypoints for route calculation, allowing multiple points and adding support for alternative routes. Update state management and event handling accordingly.

--- src/app/(pages)/ol/page.tsx
+++ src/app/(pages)/ol/page.tsx
@@ -16,11 +16,11 @@ import { LineString } from "ol/geom";
 export default function OpenLayerMap() {
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<Map | null>(null);
-  const [startPoint, setStartPoint] = useState<[number, number] | null>(null);
-  const [endPoint, setEndPoint] = useState<[number, number] | null>(null);
-  const startPointRef = useRef<[number, number] | null>(null);
-  const endPointRef = useRef<[number, number] | null>(null);
+
+  const [waypoints, setWaypoints] = useState<[number, number][]>([]);
+  const waypointsRef = useRef<[number, number][]>([]);
   const [route, setRoute] = useState<[number, number][] | null>(null);
+  const routeRef = useRef<[number, number][] | null>(null);
   const [showInstructions, setShowInstructions] = useState(true);
   const vectorSource = useRef(new VectorSource());
   const vectorLayer = useRef(
@@ -35,13 +35,14 @@ export default function OpenLayerMap() {
     })
   );
 
-  const calculateRoute = async (
-    start: [number, number],
-    end: [number, number]
-  ) => {
+  const calculateRoute = async (waypoints: [number, number][]) => {
     try {
+      const waypointsString = waypoints
+        .map((point) => `${point[0]},${point[1]}`)
+        .join(";");
+
       const response = await fetch(
-        `https://router.project-osrm.org/route/v1/driving/${start[0]},${start[1]};${end[0]},${end[1]}?overview=full&alternatives=true&steps=true&geometries=geojson`
+        `https://router.project-osrm.org/route/v1/driving/${waypointsString}?overview=full&alternatives=3&steps=true&geometries=geojson`
       );
       const data = await response.json();
       if (data.routes && data.routes[0]) {
@@ -66,6 +67,30 @@ export default function OpenLayerMap() {
             }),
           })
         );
+
+        const alternativeCoords = data.routes?.[1]?.geometry?.coordinates;
+        const alternativeRouteFeatures = alternativeCoords
+          ? new Feature({
+              geometry: new LineString(
+                alternativeCoords?.map((coord: [number, number]) =>
+                  fromLonLat(coord)
+                )
+              ),
+            })
+          : undefined;
+
+        if (!!alternativeRouteFeatures) {
+          alternativeRouteFeatures.setStyle(
+            new Style({
+              stroke: new Stroke({
+                color: "#f4f4f4",
+                width: 4,
+              }),
+            })
+          );
+          vectorSource.current.addFeature(alternativeRouteFeatures);
+        }
+
         vectorSource.current.addFeature(routeFeature);
         // Get the extent of the route feature and fit the map view to it with animation
         const extent = routeFeature.getGeometry()?.getExtent();
@@ -84,9 +109,9 @@ export default function OpenLayerMap() {
   };
 
   useEffect(() => {
-    startPointRef.current = startPoint;
-    endPointRef.current = endPoint;
-  }, [startPoint, endPoint]);
+    waypointsRef.current = waypoints;
+    routeRef.current = route;
+  }, [waypoints, route]);
 
   useEffect(() => {
     if (!mapRef.current) return;
@@ -110,31 +135,24 @@ export default function OpenLayerMap() {
       const coordinate = mapInstance.getEventCoordinate(
         event.originalEvent as MouseEvent
       );
+
+      if (!!routeRef.current) {
+        // Reset points
+        setWaypoints([]);
+        setRoute(null);
+        vectorSource.current.clear();
+      }
+
       const lonLat = transform(
         mapInstance.getCoordinateFromPixel(event.pixel as [number, number]),
         "EPSG:3857",
         "EPSG:4326"
       ) as [number, number];
-      if (!startPointRef.current) {
-        setStartPoint(lonLat);
-        const startFeature = new Feature({
-          geometry: new Point(fromLonLat(lonLat)),
-        });
-        vectorSource.current.addFeature(startFeature);
-      } else if (!endPointRef.current) {
-        setEndPoint(lonLat);
-        const endFeature = new Feature({
-          geometry: new Point(fromLonLat(lonLat)),
-        });
-        vectorSource.current.addFeature(endFeature);
-        // calculateRoute(startPointRef.current, lonLat);
-      } /*  else {
-        // Reset points
-        setStartPoint(null);
-        setEndPoint(null);
-        setRoute(null);
-        vectorSource.current.clear();
-      } */
+      setWaypoints([...waypointsRef.current, lonLat]);
+      const startFeature = new Feature({
+        geometry: new Point(fromLonLat(lonLat)),
+      });
+      vectorSource.current.addFeature(startFeature);
     });
 
     setMap(mapInstance);
@@ -146,7 +164,6 @@ export default function OpenLayerMap() {
 
   useEffect(() => {
     if (!map) return;
-
     console.log(map);
   }, [map]);
 
@@ -183,8 +200,8 @@ export default function OpenLayerMap() {
       <div className="absolute bottom-4 right-4 flex flex-col gap-2">
         <button
           onClick={() => {
-            if (startPoint && endPoint) {
-              calculateRoute(startPoint, endPoint);
+            if (waypoints.length > 0) {
+              calculateRoute(waypoints);
             }
           }}
           className=" bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
@@ -195,8 +212,7 @@ export default function OpenLayerMap() {
         <button
           onClick={() => {
             // Reset points
-            setStartPoint(null);
-            setEndPoint(null);
+            setWaypoints([]);
             setRoute(null);
             vectorSource.current.clear();
           }}
